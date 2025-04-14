f93689f51dc88957c3b6ce37d77d3813d7348814
Duda-Santosr <maria.e.santosr13@gmail.com>
2025-04-14 09:24:27 -0300

    commit

--- perfil_adm.php
+++ perfil_adm.php
@@ -84,6 +84,8 @@
                                 <option value="empty" disabled selected></option>
                                 <option value="carro">Carro</option>
                                 <option value="moto">Moto</option>
+                                <option value="caminhao">Caminhão</option>
+                                <option value="aviao">Avião</option>
                             </select>
                         </div>
                         <button class="btn btn-success w-100" type="submit" 
@@ -122,14 +124,56 @@
                             <input type="number" class="form-control" name="dias_calculo" value="1" required>
                         </div>
                         <button class="btn btn-success w-100" type="submit" 
-                        name="adicionar">
-                            Calcular
+                        name="calcular">
+                            Calcular Previsão
                         </button>
                     </form>
                 </div>
             </div>
         </div>
+     </div>
 
+     <!-- Tabela de veículos cadastrados -->
+     <div class="row mt-4">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="mb-0">
+                        Veículos Cadastrados📄
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <th>Tipo</th>
+                                <th>Modelo</th>
+                                <th>Placa</th>
+                                <th>Status</th>
+                                <th>Ações</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Carro</td>
+                                    <td>Uno</td>
+                                    <td>ABC1D34</td>
+                                    <td>
+                                        <span class="badge bg-success">
+                                            Disponível
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="action-wrapper">
+                                            
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
      </div>
 
     </div>
